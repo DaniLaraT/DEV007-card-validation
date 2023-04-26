d8eb6b0dd3472439cb7249a1dc17204b98a64778
DaniLaraT <daniela.alejandra66@gmail.com>
2023-04-25 23:04:09 -0400

    segundo

--- src/validator.js
+++ src/validator.js
@@ -8,10 +8,8 @@ const validator = {
     const inverso=arrayNumber.reverse();
     //console.log(inverso);// [9,8,7,6,5,4,3,2,1]
     //hacer que se multipliquen por 2 los numeros que estan en ubicacion par en array (1,3,7,.....)
-    for (let i = 0; i < inverso.length; i++) {
-      if (i % 2 !== 0) {
-        inverso[i] *= 2;
-      }
+    for (let i = 1; i < inverso.length; i+=2) {
+      inverso[i] *= 2;
     }
     //console.log(inverso);//[9, 16, 7, 12, 5, 8, 3, 4, 1]
     // sumar numeros de array, separando los >10 (1+0=1)
